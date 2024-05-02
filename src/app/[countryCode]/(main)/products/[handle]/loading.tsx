c2e8092c65337ac0d109119054fc60bca423a55b
Mohd Ziyad <mziyadc@gmail.com>
2024-05-02 09:23:06 +0530

    Loading page added in products/handle

---
+++ src/app/[countryCode]/(main)/products/[handle]/loading.tsx
@@ -0,0 +1,14 @@
+import { Loader2 } from "lucide-react"
+import React from "react"
+
+type Props = {}
+
+const Loading = (props: Props) => {
+  return (
+    <div className="flex w-full h-screen justify-center items-center bg-orange-100">
+      <Loader2 className="h-6 w-6 animate-spin text-amber-700" />
+    </div>
+  )
+}
+
+export default Loading
