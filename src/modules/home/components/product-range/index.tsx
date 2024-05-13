75e98a994ef49b3cc47405f78a02587584648b31
Mohd Ziyad <mziyadc@gmail.com>
2024-05-13 12:08:07 +0530

    Added navigation to product range

--- src/modules/home/components/product-range/index.tsx
+++ src/modules/home/components/product-range/index.tsx
@@ -1,5 +1,6 @@
 import { Container } from "@medusajs/ui"
 import Image from "next/image"
+import Link from "next/link"
 import React from "react"
 
 type Props = {}
@@ -9,37 +10,38 @@ const ProductRange = (props: Props) => {
     {
       name: "Doors & Window Frames",
       img: "https://i.pinimg.com/564x/39/ea/28/39ea28546fd74e5233a470cc97bcc811.jpg",
+      handle: "/collections/frames",
     },
     {
       name: "Solid Wood Doors",
       img: "https://images.unsplash.com/photo-1680472800758-b384161c9a6d?q=80&w=2875&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      handle: "/collections/doors",
     },
     {
       name: "Teak Products",
       img: "https://i.pinimg.com/564x/e4/d5/40/e4d540da20c732cf4ada2b778744efdc.jpg",
+      handle: "/collections/teak-products",
     },
-    // {
-    //   name: "Wooden Mouldings",
-    //   img: "https://i.pinimg.com/564x/d0/bb/fc/d0bbfc87485a0bb2e004129f6513cd96.jpg",
-    // },
   ]
   return (
     <div className="sm:grid flex flex-col gap-y-6 max-w-6xl px-12 m-auto gap-6 grid-cols-3">
-      {productRange.map((product, idx) => (
+      {productRange.map((item, idx) => (
         <div
           key={idx}
           className="flex flex-col transform transition-transform hover:scale-110 hover:cursor-pointer gap-y-3 text-center text-amber-700 font-semibold"
         >
-          <Container className="relative h-[300px]">
-            <Image
-              src={product.img}
-              alt="product_range"
-              layout="fill"
-              objectFit="cover"
-              className=" object-center rounded-md"
-            />
-          </Container>
-          <p>{product.name}</p>
+          <Link href={item.handle}>
+            <Container className="relative h-[300px]">
+              <Image
+                src={item.img}
+                alt="product_range"
+                layout="fill"
+                objectFit="cover"
+                className=" object-center rounded-md"
+              />
+            </Container>
+            <p>{item.name}</p>
+          </Link>
         </div>
       ))}
     </div>
